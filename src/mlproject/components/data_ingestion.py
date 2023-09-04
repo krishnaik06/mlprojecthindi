23237251779859bf44b4cebbbf24f35d900af434
Krish Naik <krishnaik06@gmail.com>
2023-09-04 22:12:07 +0530

    dataingestion changes

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -30,8 +30,8 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Data Ingestion is completed")
 
