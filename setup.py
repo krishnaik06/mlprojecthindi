01e38ac25af5a145dceb8673550575aa97160747
Kumar Shivam <kumarshivam862@gmail.com>
2023-09-30 22:35:34 +0530

    Update setup.py
    existing function is creating a list of characters like ['n','u','m','p','y']

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_requirements(file_path:str)->List[str]:
 
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace('\n',"") for req in requirements]
+        requirements=[''.join(req.replace('\n',"") for req in requirements)]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
